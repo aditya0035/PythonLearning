4d644fe3b0c98bce7b067b9cde692f60dc438514
aditya saraswat <adityasaraswat0035@gmail.com>
2018-12-18 14:11:40 +0000

    Type Casting and Getting input from user

---
+++ 08.TypeCasting.py
@@ -0,0 +1,44 @@
+'''
+Convert one type to anothe type is called typecasting
+as python is a weak type language we need to take care while performing 
+operations amoung different type so we need to cast properly
+int is used to cast a value to integer
+float is used to cast a value to float
+str is used to cast a value to string
+boolean is used to cast a value to boolean
+list is used to cast/genrate a list from 
+iterable/iterator/genrator(will discuss later)
+set is used to genrate set
+dict is used to convert a list or set to dict
+tuple is used to convert a list to tuple
+'''
+print("-----Int---")
+print(int('20'))
+print(int(20.8))
+print(int(20))
+print(int(True))
+print(int(False))
+print("----float---")
+print(float(12.0))
+print(float(12))
+print(float('20.08'))
+print(float(True))
+print(float(False))
+print("-----Str----")
+print(str(20))
+print(str(20.98))
+print(str('200'))
+print(str(True))
+print("---Bool------")
+print(bool(20))
+print(bool(''))
+print(bool({}))
+print(bool("Aditya"))
+print(bool(None))
+'''
+for other complex type we will show 
+type casting when we are going to discuss them
+'''
+
+
+print()
---
+++ 09.GettingInputFromUser.py
@@ -0,0 +1,13 @@
+'''
+For promting user to get input we use input function in earlier version 2.7 of
+python we use raw_input for getting input form user from python console
+'''
+msg = input("Please provide your name")
+
+print(msg)
+print(f'msg type:{msg}')
+
+"""
+Note:The value returned by input is a string so be careful while performing any
+operations.
+"""
