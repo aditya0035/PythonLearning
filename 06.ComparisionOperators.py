89d675c0963705fc40abe7b1c64e6810984f84d1
aditya saraswat <adityasaraswat0035@gmail.com>
2018-12-18 13:53:44 +0000

    Comparision Operators

---
+++ 06.ComparisionOperators.py
@@ -0,0 +1,17 @@
+'''
+In python below are the comparision operators
+1.> grater then a>b a grater then b
+2.< less then a<b a less then b
+3.>= grater then equal to a>=b
+4.<= less then equal to a<=b
+5.a==b equal to a==b
+6.a!=b not equal 
+These operators return a boolean value
+'''
+print(12 > 3)
+print(3 < 2)
+print(3 >= 5)
+print(3 <= 3)
+print(2 == 2)
+print(2 != 2)
+print(2 != 3)
