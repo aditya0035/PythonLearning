feae0a1986cebc4224122a93fb9f4882bd0e915d
aditya saraswat <adityasaraswat0035@gmail.com>
2018-12-18 03:40:12 +0000

    Operators details

---
+++ operators.py
@@ -0,0 +1,9 @@
+'''
+python has below operator
+1.__add__ represented as +
+2.__sub__ represented as _
+3.__multi__ represented as *
+4./ division operator
+5.// Floor Division Operator
+6.** exponatial/multiplier operator
+'''
