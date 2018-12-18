b07f317a61e669594ac3f3482322108d78405949
aditya saraswat <adityasaraswat0035@gmail.com>
2018-12-18 13:53:13 +0000

    Comparision and boolean Operators

---
+++ 07.BooleanOperators.py
@@ -0,0 +1,29 @@
+'''
+In python below are boolean operator which combine 
+two boolean values
+1.and
+2.or
+3.not
+
+Working of and operator
+If first condition is false then it will not check for 2nd condition
+and return the result of first condition ,If first is true then it will check 
+for 2nd condition and return result of 2nd value
+
+Working of or Operator
+Or operator check if first condition is True it didn't check for 2nd and return
+the result of first condition,if first condition is false the it will check for 
+2nd condition and return the result of that.
+
+Working of not Operator
+this operator negate the result that make true to false or false to true
+'''
+print(()and [])
+print(('aditya,'and []))
+print(('aditya,'and ['aditya', 'saraswat']))
+print(()or[])
+print(('aditya')or["Aditya Saraswat"])
+print(()or["Aditya Saraswat"])
+print(not())
+print(not[])
+print([]or not())
