a264902c2a023325969ff13cde9af45a3a30aea4
aditya saraswat <adityasaraswat0035@gmail.com>
2018-12-22 15:56:07 +0000

    Static Method in python

---
+++ 34.StaticMethod.py
@@ -0,0 +1,16 @@
+"""
+Static Method are those which doesnot depends on the object
+and they are called using ClassName.StaticMethod
+although we can call them from object also
+"""
+class Foo:
+    def __init__(self):
+        pass
+    @staticmethod
+    def hi(self):
+        print("Hello i don't like paramter")
+        
+
+foo=Foo()
+#foo.hi() #can't call like that as it require a parameter
+Foo.hi(foo)
