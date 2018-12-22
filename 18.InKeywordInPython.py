5dce1b81207923631591c63b14740ce90cdb06a5
aditya saraswat <adityasaraswat0035@gmail.com>
2018-12-22 06:01:21 +0000

    In Keyword Use in python

---
+++ 18.InKeywordInPython.py
@@ -0,0 +1,13 @@
+"""
+In keyword is used to check if the element exists in that iterable or not
+In keyword internally iterate over the iterable to find that element
+"""
+my_friends = ["Aditya", "Jose", "Jack", "Jimy"]
+frined_search='Ashish'
+print(frined_search in my_friends)
+frined_search='Aditya'
+print(frined_search in my_friends)
+
+"""
+Note Range(n,n) will not return anything it will be a empty list
+"""
