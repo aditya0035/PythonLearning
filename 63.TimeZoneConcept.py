2e744cfce9ccb4417783b8933d84a75b2461a47c
aditya saraswat <adityasaraswat0035@gmail.com>
2018-12-23 19:37:51 +0000

    What is timezone

---
+++ 63.TimeZoneConcept.py
@@ -0,0 +1,10 @@
+"""
+each country has their time zone so each used see the time of there local time zone
+UTC:it is global time zone we store time in that format and the then we add user Gmtoffset into that 
+to show the datetime in there local time zone
+
+Or if we know the datetime and timezone when the activity happend then we can show the other users 
+of different country the value in there time zone
+
+So timezone play an important role if we say time then we are taling about local timezone
+"""
