7b4e6a433f98a07f037335e9ba95446ab32bc48a
aditya saraswat <adityasaraswat0035@gmail.com>
2018-12-23 19:09:44 +0000

    Ordered Dictionary Collection

---
+++ 60.OrderedDictionary.py
@@ -0,0 +1,42 @@
+"""
+Ordered dictionary are part of Collection freamwork which is not a global module so we need to
+import that module
+Ordered dictionary is like dictionary but they maintain the order in which the elements are inserted
+"""
+
+from collections import OrderedDict
+
+
+dicts={"Rolf":["MIT",20],"Jose":["California",25]}
+print(dicts)
+orderedDict=OrderedDict(dicts)
+print(orderedDict)
+
+"""
+Ordered dictionary are part of Collection freamwork which is not a global module so we need to
+import that module
+Ordered dictionary is like dictionary but they maintain the order in which the elements are inserted
+"""
+
+from collections import OrderedDict
+
+
+dicts={"Rolf":["MIT",20],"Jose":["California",25]}
+print(dicts)
+orderedDict=OrderedDict(dicts)
+print(orderedDict)
+
+"""
+Ordered dictionary are part of Collection freamwork which is not a global module so we need to
+import that module
+Ordered dictionary is like dictionary but they maintain the order in which the elements are inserted
+"""
+
+from collections import OrderedDict
+
+
+dicts={"Rolf":["MIT",20],"Jose":["California",25]}
+print(dicts)
+orderedDict=OrderedDict(dicts)
+print(orderedDict)
+
