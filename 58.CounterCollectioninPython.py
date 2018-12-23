51f2c69f50d56cbcdae8ec0190d1b088857fee28
aditya saraswat <adityasaraswat0035@gmail.com>
2018-12-23 18:33:15 +0000

    Counter Collection in Python

---
+++ 58.CounterCollectioninPython.py
@@ -0,0 +1,37 @@
+"""
+Counter collection is present in collection module of python
+it is not global module so we need to import it
+Counter collection count the number of occurance of each element of the
+collection and store them in dic form
+Also we can use counter with other collection as well like 
+dict ,tuple , queue etc
+"""
+from collections import Counter
+lst=[1,2,3,4,51,2,1,23,1,2,33,4,4,67,7]
+
+count=Counter(lst)
+print(count)"""
+Counter collection is present in collection module of python
+it is not global module so we need to import it
+Counter collection count the number of occurance of each element of the
+collection and store them in dic form
+Also we can use counter with other collection as well like 
+dict ,tuple , queue etc
+"""
+from collections import Counter
+lst=[1,2,3,4,51,2,1,23,1,2,33,4,4,67,7]
+
+count=Counter(lst)
+print(count)"""
+Counter collection is present in collection module of python
+it is not global module so we need to import it
+Counter collection count the number of occurance of each element of the
+collection and store them in dic form
+Also we can use counter with other collection as well like 
+dict ,tuple , queue etc
+"""
+from collections import Counter
+lst=[1,2,3,4,51,2,1,23,1,2,33,4,4,67,7]
+
+count=Counter(lst)
+print(count)
