a8a8d937392bdf6edacb91dc2a798334a740c842
aditya saraswat <adityasaraswat0035@gmail.com>
2018-12-23 06:44:41 +0000

    Dealing with JSON files

---
+++ 40.JsonFileHandling.py
@@ -0,0 +1,28 @@
+"""
+there are two ways to json
+{
+    "<KeyName>":[{dict},{dict}]
+}
+{
+    [
+    {dict},
+    {dict}
+    ]
+}
+with out key or with key python can deal will any of this
+python have built in module for dealing with this its not global module so we need to import that
+import json
+the method are 
+json.dump() it will put the data into json file provided using filepointer
+json.loads() it will load the data into a python variable which is of type dict
+json.loads() it will load the octent of json string into a variable of dict type
+json.dumps() it will load the content  of dict type into a string variable
+"""
+import json
+filePointer=open("json.txt","r")
+content=json.load(filePointer)
+print(content)
+
+"""
+When we parse an object to JSON we need to implement the __repr__ method which should return the json string
+"""
