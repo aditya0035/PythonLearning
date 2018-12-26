3ce9aa306700881a934a5fc62e8f56249ed81606
aditya saraswat <adityasaraswat0035@gmail.com>
2018-12-26 17:00:31 +0000

    More About Yield Keyword,Priming of generator

---
+++ 81.YieldKeywordAsInputPart2.py
@@ -0,0 +1,20 @@
+from collections import deque
+from typing import Generator
+friends=deque(["Rolf","Joe","Chales","Kim","Stanley"])
+
+def friend_upper():
+    friend=friends.popleft().upper()
+    greeting=yield
+    print(f'{greeting }{friend}')
+
+def greet(g:Generator): #this will prime the generator s
+    g.send(None)
+    while True:
+        greeting=yield
+        g.send(greeting)
+
+f=friend_upper()
+greeter=greet(f)
+greeter.send(None)
+greeter.send("Good Evening ")
+
